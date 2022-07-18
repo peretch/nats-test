d37616bb9006b6e85daca54fef00cb734ce75ae1
Sebastián Pérez Etchandy <sebastian@peretch.com>
2022-07-17 21:37:09 -0300

    Added some logging of received event information:

--- src/listener.ts
+++ src/listener.ts
@@ -1,4 +1,4 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
 
 console.clear();
 
@@ -11,7 +11,21 @@ stan.on('connect', () => {
 
   const subscription = stan.subscribe('ticket:created');
 
-  subscription.on('message', (msg) => {
-    console.log('message arrived', msg);
+  subscription.on('message', (msg: Message) => {
+    // Returns the data associated with the message payload. If the stanEncoding is not
+    // set to 'binary', a string is returned.
+    const data = msg.getData();
+
+    // Returns the subject associated with this Message
+    const channel = msg.getSubject();
+
+    // Returns the sequence number of the message in the stream.
+    const sequence = msg.getSequence();
+
+    if (typeof data === 'string') {
+      console.log(
+        `Event received: \n  - number: #${sequence}\n  - channel: ${channel}\n  - data: ${data}`
+      );
+    }
   });
 });
